3fb3d3523a9d89ea139a60981e3279e7b33a3e44
dvimukthi <deshanvimukthi59@gmail.com>
2023-02-14 23:56:12 +0530

    correct questions

--- Quiz-game.py
+++ Quiz-game.py
@@ -6,21 +6,27 @@ if playing.lower() != "yes":
     quit()
 
 print("**Okay! Let's PLay** ")
+score = 0
 
 answer = input("What is CPU stand for ? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What is GPU stand for ? ")
 if answer.lower() == "Graphical processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What is RAM stand for? ")
 if answer.lower() == "Random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+print("You got" + str(score) + " questions correct!")

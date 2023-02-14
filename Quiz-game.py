9d3e73915aab9b58dd7137495ee4e8bc19a5cf52
dvimukthi <deshanvimukthi59@gmail.com>
2023-02-14 23:13:06 +0530

    lowercase

--- Quiz-game.py
+++ Quiz-game.py
@@ -1,26 +1,26 @@
 print("Welcome to Quiz Game")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play ? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("**Okay! Let's PLay** ")
 
-answer = input("What is CPU stand for? ")
-if answer == "central processing unit":
+answer = input("What is CPU stand for ? ")
+if answer.lower() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
-answer = input("What is GPU stand for? ")
-if answer == "Graphical processing unit":
+answer = input("What is GPU stand for ? ")
+if answer.lower() == "Graphical processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
 
 answer = input("What is RAM stand for? ")
-if answer == "Random access memory":
+if answer.lower() == "Random access memory":
     print("Correct!")
 else:
     print("Incorrect!")

287359ed7a01d0ae0b074ac5aa55e40be23263be
dvimukthi <deshanvimukthi59@gmail.com>
2023-02-15 23:34:21 +0530

    final auiz

--- Quiz-game.py
+++ Quiz-game.py
@@ -5,7 +5,7 @@ playing = input("Do you want to play ? ")
 if playing.lower() != "yes":
     quit()
 
-print("**Okay! Let's PLay** ")
+print("**Okay! Let's Play** ")
 score = 0
 
 answer = input("What is CPU stand for ? ")
@@ -16,17 +16,18 @@ else:
     print("Incorrect!")
 
 answer = input("What is GPU stand for ? ")
-if answer.lower() == "Graphical processing unit":
+if answer.lower() == "graphical processing unit":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What is RAM stand for? ")
-if answer.lower() == "Random access memory":
+answer = input("What is RAM stand for ? ")
+if answer.lower() == "random access memory":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-print("You got" + str(score) + " questions correct!")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 3) * 100) + "%.")
